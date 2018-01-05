a27c9f45ca70b26d0fe6d496c32b73a0cebfdd81
thedocbwarren <jedi.master@augmentedjs.com>
2018-01-05 12:25:25 -0800

    three ways to init a view

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ import Presentation from "augmentedjs-next-presentation";
 
 class MyView extends Presentation.View {
   constructor() {
-    super("ChildView");
-    this.template = `<h1>This is a simple view also.</h1><h2>My view name is ${this.name}.</h2><p>This is a simple view extends <em>Presentation.View</em></p><button id="bump">Click Me</button>`;
+    super({ "name": "ChildView" });
+    if (!this.template) {
+      this.template = `<h1>This is a simple view also.</h1><h2>My view name is ${this.name}.</h2><p>This is a simple view extends <em>Presentation.View</em></p><button id="bump">Click Me</button>`;
+    }
     this.events = {
       "click #bump": "bump"
     };
@@ -14,11 +16,44 @@ class MyView extends Presentation.View {
   };
 };
 
-const myView = new Presentation.View("MyView");
-myView.el = "#views";
+class YetAnotherView extends Presentation.View {
+  constructor(options) {
+    console.log("options", options);
+    super(options);
+    console.log("im here", this.name);
+    if (!this.template) {
+      console.log("oops no template");
+      this.template = `oops`;
+    }
+    if (!this.events) {
+      console.log("oops no events");
+      this.events = {
+        "click #bump2": "bump"
+      };
+    }
+
+  };
+  bump(e) {
+    alert("I was clicked!");
+  };
+};
+
+const myView = new Presentation.View({ "name": "MyView" });
+myView.el = "#view1";
 myView.template = `<h1>This is a simple view.</h1><h2>My view name is ${myView.name}.</h2><p>This is a simple view known as <em>Presentation.View</em></p><p>My permissions are${JSON.stringify(myView.permissions)}</p>`;
 myView.render();
 
 const anotherView = new MyView();
 anotherView.render();
 anotherView.delegateEvents();
+
+const yetAnotherView = new YetAnotherView({
+  "el": "#view2",
+  "name": "YetAnotherView",
+  "template": "<h1>This is a simple view.</h1><h2>My view name is YetAnotherView.</h2><p>This vew is declared by passing options.</p><button id=\"bump2\">Click Me</button>",
+  "events": {
+    "click #bump2": "bump"
+  }
+});
+yetAnotherView.render();
+yetAnotherView.delegateEvents();
