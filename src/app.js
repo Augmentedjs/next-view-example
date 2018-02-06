f2730c11a244fc889eee1761008ffd1ed3cc905e
thedocbwarren <jedi.master@augmentedjs.com>
2018-02-06 05:10:04 -0800

    new views

--- src/app.js
+++ src/app.js
@@ -4,3 +4,12 @@ import Router from "./router.js";
 const app = new Presentation.Application("next-view-example");
 app.router = new Router();
 app.start();
+
+const article = new Presentation.Component.Article({ "template": "hello"});
+article.render();
+
+const header = new Presentation.Component.Header({ "template": "hello"});
+header.render();
+
+const ham = new Presentation.Component.HamburgerMenu();
+ham.render();
